099ec12ba7592a45c765c57ea18fc6e5e006e136
Troy Kurniawan <troy.kurniawan@sjsu.edu>
2019-03-28 15:55:16 -0700

    Updated to Luke's Branch

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,19 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.helloPubSub = functions.pubsub.topic('events').onPublish((message) => {
+    // name = message.json.name;
+    // const messageBody = message.data ? Buffer.from(message.data, 'base64').toString() : null;
+    // console.log(messageBody);
+    let name = null;
+    try {
+        // name = message.json.name;
+        console.log(message.json);
+    } catch (e) {
+        console.error('PubSub message was not JSON', e);
+    }
+    return admin.database().ref("/testCloudFunc").set({
+        this: "iotworked"
+    });
+  });
