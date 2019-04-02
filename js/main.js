21eb7a02809eb86b2143aa56a6ba9fe1526ccd7c
Troy Kurniawan <troy.kurniawan@sjsu.edu>
2019-04-01 17:37:54 -0700

    Sidebar table added/favicon added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 // ==== GLOBALS ==============================================
 var map;
 var colon_tick = false;
+var markerCount = 0;
+var markersArray = [];
+var squadArray = [];
+var colorNum = 0;
+let color = ["blue", "pink", "green", "purple", "yellow",
+"white", "brown", "gray", "orange", "black"];
+var floorInputVal = 0;
+var newMapsArray = [];
+var newDivsArray = [];
+var beenPressed = 0;
 
 // ==== IMPORTANT APIS/INITS =================================
 
@@ -50,7 +60,7 @@ function initMap()
     map = new google.maps.Map(document.getElementById("map"), myOptions);
 };
 
-// ==== HELPER FUNCTIONS =====================================
+// ==== SMALLER FUNCTIONS =====================================
 
 // Time Function (Updates every 0.5s)
 setInterval(updateTime, 1000);
@@ -95,6 +105,130 @@ updateTime();
 function toggleSidebar()
 {
     document.getElementById("sidebar").classList.toggle('active');
+}
+
+// ==== ADDING MEMBERS TO TABLE =====================================
+
+// Member class to hold properties of each worker
+class squadMember
+{
+    constructor(name, status, floor, marker, color, lat, long, startTime, squad, heart, temp)
+    {
+        this.squad = squad;
+        this.name = name;
+        this.status = status;
+        this.floor = floor;
+        this.marker = marker;
+        this.color = color;
+        this.lat = lat;
+        this.long = long;
+        this.heart = heart;
+        this.temp = temp;
+        this.startTime = startTime;
+    }
+}
+
+function importSquad()
+{
+    // get the name that was entered
+    var sqInputVal = document.getElementById('squadInput').value;
+
+    // get time
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+
+    var inactiveRef = firebase.database().ref("All_Squads/" + sqInputVal);
+    var newSquadMember;
+    inactiveRef.on("value", function(snapshot) {
+        const obj = snapshot.val();
+        for (const key in obj) 
+        {
+            newSquadMember = new squadMember(obj[key].Member, "Good", 1, 
+                addAMarker(obj[key]), color[colorNum], 37.337032, -121.880224, 
+                time, obj[key].Squad, 50, 100);
+            console.log(newSquadMember);
+
+            // put new member into squad array
+            squadArray.push(newSquadMember);
+
+            // put the new member into the table
+            placeInTable(newSquadMember);
+
+            // put new member into firebase
+            activateMember(newSquadMember);
+            markerCount++; 
+        }
+    });     
+    colorNum++;
+}
+
+function placeInTable(squadObj)
+{
+    // Find a <table> element
+    var table = document.getElementById("squadTable");
+
+    // Create an empty <tr> element and add it to the 1st position of the table:
+    var row = table.insertRow(1);
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);      // squad
+    var cell2 = row.insertCell(1);      // name
+    // var cell3 = row.insertCell(2);      // status
+    // var cell4 = row.insertCell(3);      // floor
+    // var cell5 = row.insertCell(4);      // color
+    // var cell6 = row.insertCell(5);      // heart
+    // var cell7 = row.insertCell(6);      // temp
+    // var cell8 = row.insertCell(7);      // time
+
+    // Add some text to the new cells:
+    cell1.innerHTML = squadObj.squad;
+    cell2.innerHTML = squadObj.name;
+    // cell3.style.color = "limegreen";
+    // cell3.innerHTML = squadObj.status;
+    // cell4.innerHTML = squadObj.floor;
+    // cell5.innerHTML = squadObj.color;
+    // cell6.innerHTML = squadObj.heart;
+    // cell7.innerHTML = squadObj.temp;
+    // cell8.innerHTML = squadObj.startTime;
+}
+
+// takes in the squad object and throws it into firebase
+function activateMember(newSquadMember)
+{
+    firebase.database().ref("Active/" + newSquadMember.name).set({
+        name: newSquadMember.name,
+        squad: newSquadMember.squad,
+        status: newSquadMember.status,
+        floor: newSquadMember.floor,
+        marker: newSquadMember.color,
+        lat: newSquadMember.lat,
+        long: newSquadMember.long,
+        markerNum: markerCount
+    });
+}
+
+function addAMarker(squadObj)
+{
+    // add a marker to the map
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(37.337032, -121.880224),
+        title: squadObj.Squad + squadObj.Member,
+        draggable: false,
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 7,
+            fillColor: color[colorNum],
+            fillOpacity: 0.8,
+            strokeWeight: 1
+        }
+    });
+
+    // add marker to the array
+    markersArray.push(marker);
+
+    // adding the marker to the map
+    marker.setMap(map);
+    return marker;
 }
 
 // ==== GPS COORDINATES ======================================
