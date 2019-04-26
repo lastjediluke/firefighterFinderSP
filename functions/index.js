744c9cfef7d77669842a3d17edae5002635a74bd
Luke Dillon <luke.k.dillon@gmail.com>
2019-04-26 12:07:54 -0700

    a push

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -10,17 +11,46 @@ admin.initializeApp();
 // });
 
 exports.helloPubSub = functions.pubsub.topic('events').onPublish((message) => {
-    // name = message.json.name;
-    // const messageBody = message.data ? Buffer.from(message.data, 'base64').toString() : null;
-    // console.log(messageBody);
+
+    // turns it into base64...this one works
+    const messageBody = message.data ? Buffer.from(message.data, 'base64').toString() : null;
+    
     let name = null;
+    let squad = null;
+    let lat = null;
+    let long = null;
+    let temp = null;
+    let floor = null;
+    let status = null;
+    let gps = null;
+    
+    console.log(messageBody);
+    console.log(message);
     try {
-        // name = message.json.name;
-        console.log(message.json);
+        // json = JSON.parse(message);
+        name = message.json.name;
+        squad = message.json.squad;
+        // lat = message.json.lat;
+        // long = message.json.long;
+        // temp = message.json.temp;
+        // floor = message.json.floor;
+        // status = message.json.status;
+        // gps = message.json.gps;
+        // console.log(gps);
+        console.log(name);
+        console.log(squad);
+        // console.log(lat);
+        // console.log(long);
+        console.log(message);
     } catch (e) {
         console.error('PubSub message was not JSON', e);
     }
-    return admin.database().ref("/testCloudFunc").set({
-        this: "iotworked"
+
+    return admin.database().ref("/Active/" + name).update({
+        // lat: lat,
+        // long:long,
+        // temp:temp,
+        floor: 2
+        // status: status
     });
   });
--- js/main.js
+++ js/main.js
@@ -366,7 +366,17 @@ squadRef.on("child_changed", function(snapshot) {
 // 37.336708, -121.879543 = Southeast
 
 
- 
+var GPS = require('../gps.js');
+var gps = new GPS;
+
+var sentence = '$GPGGA,224900.000,4832.3762,N,00903.5393,E,1,04,7.8,498.6,M,48.0,M,,0000*5E';
+
+gps.on('data', function(parsed) {
+
+  console.log(parsed);
+});
+
+gps.update(sentence);
 
 
 
